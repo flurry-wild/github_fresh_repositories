dc465a504e24e8a09bc7323edf4c38f59159d466
maxim246 <maxim.mishanin41@yandex.ru>
2021-07-25 16:38:09 +0400

    first-version

---
+++ controllers/GithubController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\GithubRepository;
+use app\models\GithubUser;
+use yii\helpers\Url;
+use yii\web\Controller;
+use Yii;
+
+class GithubController extends Controller
+{
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $githubUsers = GithubUser::find()->all();
+
+        return $this->render('index', compact('githubUsers'));
+    }
+
+    public function actionStore()
+    {
+        $userName = Yii::$app->request->post('user');
+
+        GithubUser::create($userName);
+
+        Yii::$app->response->redirect(Url::to('/github/index'));
+    }
+
+    /**
+     * @return string
+     */
+    public function actionRepositoryIndex()
+    {
+        $repositories = GithubRepository::find()
+            ->orderBy(['last_update' => SORT_DESC])
+            ->limit(10)
+            ->all();
+
+        return $this->render('/github-repository/index', compact('repositories'));
+    }
+}
